9b3e5712fb665aedb952754327dbd68e619752bf
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2018-11-19 08:52:32 +0900

    a

---
+++ DNT-VPN-JSONRPC2-CS-ORIGIN/DNT-VPN-JSONRPC2-CS-ORIGIN/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SoftEther.JsonRpc;
+using SoftEther.VPNServerRpc;
+
+namespace DNT_VPN_JSONRPC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VPNRPCTest test = new VPNRPCTest();
+
+            int i = 0;
+
+            for (i = 0; ; i++)
+            {
+                Console.WriteLine("-------------");
+                Console.WriteLine($"Test #{i} start");
+                test.Test_All();
+                Console.WriteLine($"Test #{i} finish");
+                Console.WriteLine("-------------");
+            }
+
+            //Tools.GenCode1();
+
+            //RpcServerInfo a = r.GetServerInfoAsync().Result;
+
+            //a.Print();
+
+
+
+            //VpnRpcSetUser a = new VpnRpcSetUser(){
+
+        }
+    }
+}
